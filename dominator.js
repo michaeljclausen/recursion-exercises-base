24f21a0336ecb09859578d86b613aed38afc7f71
Michael Clausen <michaeljclausen@gmail.com>
2017-05-10 16:34:40 -0700

    add getElementById()

--- dominator.js
+++ dominator.js
@@ -2,7 +2,14 @@ const { flattenTreeToArray } = require('./dom-util');
 const _ = require('underscore');
 
 const getElementById = function(root, id) {
-  // Your code here
+  let arr = flattenTreeToArray(root);
+  let result;
+  arr.forEach(el => {
+    if (el.id === id) {
+      result = el;
+    }  
+  });
+  return result;
 };
 
 const getElementsByClassName = function(root, className) {
